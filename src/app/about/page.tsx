96e7a19b3dc0b1612b8da3ba0404650c194dd0cd
Renée Draper <reneee.draper@gmail.com>
2024-07-11 20:42:30 -0600

    (app) Organize file structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,19 @@
 'use client'
-import React, { useState } from "react"
+import React from "react"
 
 export default function AboutMe() {
-    const [isOpen, setIsOpen] = useState(false)
-
-    const handleOnClick = () => {
-        setIsOpen(!isOpen)
-    }
     return (
         <div className="box">
-            <p className="title is-size-0 is-size-1-mobile">ABOUT ME</p>
+            <header className="title is-size-0 is-size-1-mobile">ABOUT ME</header>
             <p className="subtitle is-size-3 is-size-4-mobile has-text-color-dark-gray">I like making silly things</p>
-            <p>Hi! I'm Renée Draper. I enjoy creating all sorts of things, from silly websites like this to collages to moth costumes.
+            <p className="mt-3">Hi! I'm Renée Draper. I enjoy creating all sorts of things, from silly websites like this to collages to moth costumes.
                 If I'm not spending my time making something, I'm usually lifting weights, roller skating, going on wee walkabouts, or something else active.</p>
             <br></br>
             <p>When it comes to coding, I really enjoy front-end work, which certainly can't come as a surprise. I care about clean code and architecture,
                 responsive and accessible designs, and always pushing myself to learn new things.</p>
-            <p className="has-text-weight-bold mt-4 p-2 pretty-border-black">"Renée so so good at coding, plus she's just a lovely person to have around" -Robert C. Martin 
-            <span className="is-clickable" onClick={handleOnClick}>*</span></p>
-            {isOpen ?<p className="is-size-7 mt-4 p-2 ">*Ok, fine he didn't say this...probably.</p> : <span></span>}
+            <p className="has-text-weight-bold mt-4 p-2 pretty-border-black">"Renée is so so good at coding, plus she's just a lovely person to have around" -Robert C. Martin
+                <span className="is-clickable" >*</span></p>
+            <p className="is-size-7 mt-4 p-2 ">*Ok, fine he didn't say this...probably.</p>
 
         </div>
 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,59 +2,65 @@
 import React from "react"
 
 import Link from 'next/link';
+import Card from "../_components/card";
+import GetCompliment from "../_components/compliment/compliment-generator";
+import { useState } from 'react';
 
 
 export default function Projects() {
+
+
     return (
         <>
-            <div className="columns">
-                <div className="column">
-                    <div className="box has-text-black content">
-                        <p className="title is-size-0 is-size-1-mobile is-uppercase">Projects</p>
-                        <p className="subtitle is-size-4 is-size-5-mobile mb-1">
-                            Code Related Things
-                        </p>
-                        <ul className="mt-1">
-                            <li className="li">
-                                <Link href="projects/compliment">
-                                Leslie Knope Compliment Generator
-                                </Link>
-                            </li>
-                            <li className="li" >
-                                <Link href="projects/chomp">Chomper aka Crunchatize Me Cap'n</Link>
-                            </li>
-                            <li className="li" >
-                                <span>This silly lil website. It started from the compliment generator I built years ago using 
-                                    create-react-app because I wanted to play with Cloudflare workers. Then I got laid 
-                                    off a few weeks ago (in May 2024) and spent a couple days making this website to reorient myself with React after working with Angular
-                                    for a year. It's still pretty basic, but keep coming back for new updates!
-                                </span>
-                            </li>
-                        </ul>
-                        <p className="subtitle mb-1 mt-3">
-                            Coming Soon!
-                        </p>
-                        <ul>
-                            <li>
-                                Mood-based theme picker
-                            </li>
-                            <li>
-                                silly pure css things (like a famous painting entirely of css)
-                            </li>
-                            <li>
-                                rewriting this site with next.js (and typescript because I prefer it)
-                            </li>
-                            <li>
-                                adding more fun animations with React Spring
-                            </li>
-                             <li>
-                                More accessibility features (large text options, screeen reader friendly)
-                            </li>
-                        </ul>
-                    </div>
+            <div className="box has-text-black content">
+                <p className="title is-size-0 is-size-1-mobile is-uppercase">Projects</p>
+                <p className="subtitle is-size-4 is-size-5-mobile mb-1">
+                    Code Snippets
+                </p>
 
-                </div>
-            </div>
+
+                <Card title="Leslie Knope Compliment Generator" content={GetCompliment()}>
+                </Card>
+
+
+                {/* <a aria-roledescription="button" href="https://codesandbox.io/p/sandbox/leslie-knope-compliment-generator-f7mq88?file=%2Fsrc%2FApp.tsx" target="_blank" className="button is-warning mt-3">
+                    <span>Crunchatizer/Chompers Code</span>
+                    <span className="icon material-symbols-outlined is-size-5">open_in_new</span>
+                </a> */}
+
+
+
+
+                <p className="subtitle is-size-4 is-size-5-mobile mb-1 mt-3">
+                    Current Projects
+                </p>
+                <ul>
+                    <li>
+                        <span>This silly lil website. It started from the compliment generator I built years ago using
+                            create-react-app because I wanted to play with Cloudflare workers. Then I got laid
+                            off a few weeks ago (in May 2024) and spent a couple days making this website to reorient myself with React after working with Angular
+                            for a year. It's still pretty basic, but keep coming back for new updates!
+                        </span>
+                    </li>
+                    <li>
+                        Mood-based theme picker
+                    </li>
+                    <li>
+                        silly pure css things (like a famous painting entirely of css)
+                    </li>
+                    <li>
+                        rewriting this site with next.js (and typescript because I prefer it)
+                    </li>
+                    <li>
+                        adding more fun animations with React Spring, playing with three.js
+                    </li>
+                    <li>
+                        More accessibility features (large text options, screeen reader friendly)
+                    </li>
+                </ul>
+
+            </div >
+
         </>
     )
 }
