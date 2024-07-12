f1678dca3d4abc0cfc6bb3acf8d65c38e84df74d
Renée Draper <reneee.draper@gmail.com>
2024-07-11 21:27:48 -0600

    (css) Add rotate animation for future use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,6 @@ export default function Home() {
         <header className="title is-size-0 is-size-1-mobile">WELCOME!</header>
         <p className="subtitle is-size-3 is-size-4-mobile has-text-color-dark-gray mb-3">{`I\'M SO GLAD YOU\'RE HERE`}</p>
         <p className="is-size-5 mt-5">{`Congratulations! You\'ve found the best place on the internet.`}
-          <svg className="svg-shapes color shape1 rotate" width="85" height="85" viewBox="0 0 194 194" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M192 83.0024H131.162L174.181 39.9832L154.17 19.972L111.151 62.9912V2H82.8494V62.9912L39.8302 19.972L19.819 39.9832L62.8382 83.0024H2V111.304H62.8382L19.819 154.323L39.8302 174.334L82.8494 131.315V192H111.151V131.315L154.17 174.334L174.181 154.323L131.162 111.304H192V83.0024Z" stroke="black" stroke-width="4" stroke-miterlimit="10"></path>
-          </svg>
         </p>
       </div>
       <div className="is-max-width-800">
@@ -20,7 +17,11 @@ export default function Home() {
             <path fill="#EF444E" d="M571.836,470.564h-15.562c0-68.69-55.582-124.273-124.273-124.273c-68.69,0-124.273,55.582-124.273,124.273  h-15.563c0-77.292,62.544-139.836,139.836-139.836S571.836,393.272,571.836,470.564z" />
             <path fill="#E0E9F6" d="M594.572,472.801c19.889-18.988-13.354-37.844-27.658-20.111c-12.507-18.931-47.823-14.388-53.199,8.623  c-9.004-15.539-44.177-11.515-31.499,20.74c-28.95,8.817-3.471,59.468,28.023,36.498c8.921,22.698,67.744,17.712,73.958-5.136  C610.886,516.88,625.574,482.085,594.572,472.801z" />
             <path fill="#E0E9F6" d="M269.427,472.801c-19.888-18.988,13.355-37.844,27.659-20.111c12.506-18.931,47.823-14.388,53.199,8.623  c9.004-15.539,44.177-11.515,31.499,20.74c28.951,8.817,3.471,59.468-28.023,36.498c-8.921,22.698-67.744,17.712-73.958-5.136  C253.115,516.88,238.425,482.085,269.427,472.801z" /></g>
+        </svg>
+        <svg className="svg-shapes color shape1 rotate" width="85" height="85" viewBox="0 0 194 194" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M192 83.0024H131.162L174.181 39.9832L154.17 19.972L111.151 62.9912V2H82.8494V62.9912L39.8302 19.972L19.819 39.9832L62.8382 83.0024H2V111.304H62.8382L19.819 154.323L39.8302 174.334L82.8494 131.315V192H111.151V131.315L154.17 174.334L174.181 154.323L131.162 111.304H192V83.0024Z" stroke="black" stroke-width="4" stroke-miterlimit="10"></path>
         </svg></div>
+
 
     </>)
 
