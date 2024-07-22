258f0f90b5f42c82990dd1b5f11e5afb83d31141
Renée Draper <reneee.draper@gmail.com>
2024-07-22 16:58:29 -0600

    (components) Add header animation for card

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -5,41 +5,50 @@ import { useSpring, animated, config } from '@react-spring/web'
 export default function ExpandableCard({ title = '', content = <div></div> }) {
     const [isExpanded, setIsExpanded] = useState(false)
 
-    const [styles, api] = useSpring(() => ({
+    const [cardStyles, animtateCardHeight] = useSpring(() => ({
         from: {
-            y: 0,
-            maxHeight: 0
+            maxHeight: isExpanded ? 1000 : 0
+        }
+    }))
+
+    const [headerStyles, animateHeaderRadius] = useSpring(() => ({
+        from: {
+            borderBottomLeftRadius: isExpanded ? 0 : 12,
+            borderBottomRightRadius: isExpanded ? 0 : 12
         }
     }))
 
     const expandOrCollapse = () => {
         setIsExpanded(!isExpanded)
-        api.start({
-            from: {
-                y: !isExpanded ? -12 : 0,
-                maxHeight: isExpanded ? 1000 : 0
-            },
+        animateHeaderRadius.start({
+            to:
+            {
+                borderBottomLeftRadius: isExpanded ? 12 : 0,
+                borderBottomRightRadius: isExpanded ? 12 : 0
+            }
+        })
+        animtateCardHeight.start({
             to: {
-                y: isExpanded ? -12 : 0,
                 maxHeight: !isExpanded ? 1000 : 0
-            },
+            }
         })
     }
 
+
+
     return (
         <>
-
-            <div className="card">
-                <header className="card-header is-shadowless is-clickable" onMouseDown={expandOrCollapse}>
+            <div className="card mb-3">
+                <animated.header className="card-header is-shadowless is-clickable has-background-danger-light" style={{ ...headerStyles }} onMouseDown={expandOrCollapse}>
                     <p className="card-header-title mb-0">{title}</p>
                     <button className="card-header-icon" aria-label="more options" >
                         <span className="icon material-symbols-outlined" aria-hidden="true">
                             keyboard_arrow_down
                         </span>
                     </button>
-                </header >
+                </animated.header >
                 <div className="card-content p-0 m-0" >
-                    <animated.div style={{ overflow: "hidden", ...styles }} >
+                    <animated.div style={{ overflow: "hidden", ...cardStyles }} >
                         {content}
                     </animated.div>
                 </div>
